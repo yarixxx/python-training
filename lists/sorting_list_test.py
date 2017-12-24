7f96813464cb563cc2374ba491f70e46fb08f921
Iaroslav Karandashev <yarix7@gmail.com>
2017-12-23 18:58:33 -0800

    Shorten list name

--- lists/sorting_list_test.py
+++ lists/sorting_list_test.py
@@ -3,18 +3,18 @@ import unittest
 class SortingListTest(unittest.TestCase):
 
     def test_selection_sort(self):
-	original_list = [8,4,9,2,5,3,1]
-	n = len(original_list)
+	s_list = [8,4,9,2,5,3,1]
+	n = len(s_list)
 	for i in range(0, n):
 	    min_index = -1
 	    for j in range(i, n):
-		if original_list[j] < original_list[min_index]:
+		if s_list[j] < s_list[min_index]:
 		    min_index = j
 	    if i != min_index:
-		original_list[i], original_list[min_index] = original_list[min_index], original_list[i]
+		s_list[i], s_list[min_index] = s_list[min_index], s_list[i]
 	
 	for index in range(1, n): 
-	    self.assertLess(original_list[index - 1], original_list[index])
+	    self.assertLess(s_list[index - 1], s_list[index])
 	
 
 if __name__ == '__main__':
