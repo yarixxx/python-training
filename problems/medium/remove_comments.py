1ff73626c4cbae09c4416c14e9f04fbf936d355d
Iaroslav Karandashev <yarix7@gmail.com>
2018-02-19 16:30:53 -0800

    add problem remove comments

---
+++ problems/medium/remove_comments.py
@@ -0,0 +1,22 @@
+import unittest
+
+class RemoveCommentsTest(unittest.TestCase):
+
+    def test_assert_remove_comments(self):
+    	self.assertEqual(self.remove_comments('text /* asdf */ qwer'), 'text  qwer')
+        self.assertEqual(self.remove_comments('before /* comment */ after /* comment 2 */ qwer'), 'before  after  qwer')
+
+    def remove_comments(self, text):
+        final_text = list()
+        parts = text.split('/*')
+        for i, part in enumerate(parts):
+            subpart = part.split('*/')
+            if len(subpart) == 2:
+                final_text.append(subpart[1])
+            if len(subpart) == 1:
+                final_text.append(subpart[0])
+        return ''.join(final_text)
+                
+
+if __name__ == '__main__':
+    unittest.main()
