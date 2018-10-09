8e834adf0fdf26ce515260e6d14999743fa86aa7
Iaroslav Karandashev <yarix7@gmail.com>
2018-10-09 10:24:07 -0700

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -4,7 +4,7 @@ class MyFirstTest(unittest.TestCase):
 
     def test_assert_something(self):
         self.assertEqual(1, 1)
-	self.assertTrue(True)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
